9284c7bc62e2ce0005a49456703698e5e81cdc86
zimbatm <zimbatm@zimbatm.com>
2013-12-03 23:06:49 +0000

    Simplify the uuid lib a bit

--- unique_id.go
+++ unique_id.go
@@ -6,31 +6,26 @@ import (
 	"time"
 )
 
-var uuid *UUID
+var uuid UUID
 
-type UUID struct {
-	id chan string
-}
+type UUID chan string
 
-func (self *UUID) Generate(N int) (uuid string) {
-	uuid = <-self.id
+func (self UUID) Generate(N int) (uuid string) {
+	uuid = <-self
 	return uuid[0:(N - 1)]
 }
 
-func (self *UUID) run() {
+func (self UUID) run() {
 	h := sha1.New()
 	c := []byte(time.Now().String())
 
 	for {
 		h.Write(c)
-		self.id <- fmt.Sprintf("%x", h.Sum(nil))
+		self <- fmt.Sprintf("%x", h.Sum(nil))
 	}
 }
 
 func init() {
-	uuid = &UUID{
-		id: make(chan string),
-	}
-
+	uuid = make(chan string)
 	go uuid.run()
 }
